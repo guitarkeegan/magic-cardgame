e3965767cc6c9644b848785ef6848e47b507537e
guitarkeegan <keegananglim@gmail.com>
2023-05-15 21:53:56 -0700

    cleaned json for white cards

--- src/cleaner.ts
+++ src/cleaner.ts
@@ -1,8 +1,26 @@
 import fs from "fs";
 
 async function main(){
-    const whiteCards = fs.readFileSync("white.json", "utf8");
-    const jsonCards = JSON.parse(whiteCards);
+    let whiteCards = fs.readFileSync("white.json", "utf8");
+    let cardsObj = JSON.parse(whiteCards);
+    console.log("removing types");
+    
+
+    const toRemove = ["type", "subtypes", "rarity", "set", "setName", "layout", "setName", "multiverseid", "rulings", "foreignNames", "originalText", "originalType", "printings", "superTypes", "variations", "legalities"];
+    
+
+    function fieldsToRemove(jsonObj: Record<string,any>[], fields: string[]): Record<string,any> {
+        for (let card of jsonObj){
+            for (let field of fields){
+                delete card[field]
+            }
+        } 
+        return jsonObj;
+    }
+
+    const newObj = fieldsToRemove(cardsObj.cards, toRemove);
+
+    console.log(newObj);
 }
 
 main()
